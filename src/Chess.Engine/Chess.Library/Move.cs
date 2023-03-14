945b6235a6f8454ff0a0bd2b1290a560320fe85a
jonathanclausen <jonathan97clausen@gmail.com>
2023-03-14 18:54:00 +0100

    introduced move class

---
+++ src/Chess.Engine/Chess.Library/Move.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Chess.Library
+{
+  public class Move
+  {
+    public Spot Start { get; private set; }
+    public Spot To { get; private set; }
+    public Move()
+    {
+    }
+  }
+}
+
