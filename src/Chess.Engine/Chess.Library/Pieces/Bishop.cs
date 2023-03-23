1fc3cffbbd87dacbe4df693915741680b83e151c
jonathanclausen <jonathan97clausen@gmail.com>
2023-03-23 19:45:20 +0100

    bishop begun

---
+++ src/Chess.Engine/Chess.Library/Pieces/Bishop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Chess.Library.Pieces
+{
+  public class Bishop : Piece
+  {
+    public Bishop(Color color) : base(color) { }
+
+    public override bool CanMove(Board board, Spot start, Spot end)
+    {
+      var distX = Math.Abs(end.Position.X - start.Position.X);
+      var distY = Math.Abs(end.Position.Y - start.Position.Y);
+
+      if (distX != distY)
+        return false; // can only move diagonal
+      if (end.HasPiece() && end.Piece.Color == Color)
+        return false; 
+      
+      return true;
+    }
+  }
+}
+
